64e2176826ecd44a3d4efed23d6608e30fa6b837
Vera Zago <veranyc@gmail.com>
2017-04-13 21:18:39 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,15 @@
 def unsafe?(speed)
-
+  if speed > 60 || speed < 40
+    unsafe = true
+  else
+    unsafe = false
+  end
+  return unsafe
 end
 
 
 
 def not_safe?(speed)
-	
+  speed > 60 || speed < 40 ? not_safe = true : not_safe = false
+  return not_safe
 end
-	
-
-
